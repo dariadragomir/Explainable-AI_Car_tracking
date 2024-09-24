b07938743a6971214be2be3a05caf284e0f365c1
Daria <82290545+dariadragomir@users.noreply.github.com>
2024-09-24 23:18:16 +0300

    exlaniable ai using lime

--- Explainable AI/xai.py
+++ Explainable AI/xai.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import torch.nn as nn
-import numpy as np
+import numpy as np 
 import os, json
 import torch
 from torchvision import models, transforms
